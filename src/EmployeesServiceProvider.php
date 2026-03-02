2d68fededcdf872ba8ce825525afd3b0266d93f2
travelxcoder3 <coder3.travelx@gmail.online>
2026-03-02 12:52:14 +0300

    Fix: hr/employees prefix

--- src/EmployeesServiceProvider.php
+++ src/EmployeesServiceProvider.php
@@ -26,7 +26,7 @@ class EmployeesServiceProvider extends ServiceProvider
             'company.domain',
             \Athka\Saas\Http\Middleware\SetCompanyTimezone::class,
         ])
-            ->prefix('staff')
+            ->prefix('hr/employees')
             ->name('company-admin.employees.')
             ->group(__DIR__ . '/Routes/web.php');
 
